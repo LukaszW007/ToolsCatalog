a3f5df49902afda8524a2d7bf5f91359459e14fb
LukaszWiszniewski <wisznu07@gmail.com>
2018-10-16 19:44:47 +0200

    Configured webpack

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App.js';
+import {AppContainer} from 'react-hot-loader';
+
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('app'),
+    )
+};
+
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./containers/App.js', () => {
+        render(App)
+    })
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,73 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const env = process.env.NODE_ENV || 'development';
 
-module.exports={
-    entry: './src/app.js',
-    output:{
+var plugins = [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })
+];
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    );
+}
+module.exports = {
+    entry: ['react-hot-loader/patch', './src/index.js'],
+    output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
-    }
-    module:{
+    },
+    devtool: 'cheap-module-eval-source-map',
+    module: {
         rules: [
             {
                 test: /\.js$/,
-                loader: 'babel-loader'
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: ['es2015', 'react'],
+                    plugins: ["react-hot-loader/babel"]
+                }
+            },
+            //CSS loader, you should install package by: npm install --save-dev style-loader css-loader
+            //documentation: https://webpack.js.org/loaders/sass-loader/
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'sass-loader'}
+                ]
             }
         ]
-    }
-}
+    },
+    devServer: {
+        contentBase: './build',
+        hot: true
+    },
+    plugins
+
+
+};
