498b695d70deb2cb134ba4c1d007ee00259d65a0
LukaszWiszniewski <wisznu07@gmail.com>
2018-11-15 17:18:09 +0100

    Added mysql connectivity

--- src/server.js
+++ src/server.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+// const bodyParser=require('body-parser');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'mysql.cba.pl',
+    user: 'cowi',
+    password: 'Cowi18',
+    database: 'cowi'
+});
+const app = express();
 
-const app =express();
-const server = http.createServer(app);
-const io =socketio(server);
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname+'/public'));
+app.get('/', function (req, res) {
+    connection.connect();
 
-app.get ('/',function(req,res){
-    res.sendFile(__dirname+'/index.html')
+    connection.query('SELECT * FROM users', (error,results,fields)=>{if (error) throw error;
+        res.send(results)});
+    connection.end();
+
 });
 
-server.listen(3000,function () {
+server.listen(3000, function () {
     console.log('listening on: 3000')
 });
