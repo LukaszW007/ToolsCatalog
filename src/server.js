30c6ec902546326c9cd513bb75d02656ac0715c3
LukaszWiszniewski <wisznu07@gmail.com>
2018-11-15 18:14:56 +0100

    Fixed simple mysql connectivity to local mysqlDB

--- src/server.js
+++ src/server.js
@@ -2,24 +2,27 @@ const express = require('express');
 // const bodyParser=require('body-parser');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'mysql.cba.pl',
-    user: 'cowi',
-    password: 'Cowi18',
-    database: 'cowi'
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'tollscatalog'
 });
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     connection.connect();
 
-    connection.query('SELECT * FROM users', (error,results,fields)=>{if (error) throw error;
-        res.send(results)});
+    connection.query('SELECT * FROM tools', (error, results, fields) => {
+        if (error) throw error;
+        res.send(results);
+        console.log(results);
+    });
     connection.end();
 
 });
 
-server.listen(3000, function () {
+app.listen(3000, function () {
     console.log('listening on: 3000')
 });
